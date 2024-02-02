c324cb4a30e7070027e87fa65da787646414ec66
Ailton Duarte <adidas.coyas@gmail.com>
2024-02-02 12:30:42 -0100

    feat: news view

--- kriolstock_dapi/assets/urls.py
+++ kriolstock_dapi/assets/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from assets.views import DocumentosView #, DocumentoCreateView
+from assets.views import DocumentoDetailView , DocumentoListView
+
+from django.urls import path
 
 urlpatterns = [
-    # path('docs/', DocumentosView.as_view(), name='listar_documentos'),
-    # path('assets', DocumentosView.as_view),
-    path('documentos/', DocumentosView.as_view(), name='documentos-list'),  # Para GET e POST
-    # path('document-list/', DocumentosView.as_view()),
-    # path('document-add/', DocumentoCreateView.as_view()),
+    path('documentos/', DocumentoListView.as_view()),
+    path('documentos/<int:documento_id>/', DocumentoDetailView.as_view()),
 ]
--- kriolstock_dapi/assets/views.py
+++ kriolstock_dapi/assets/views.py
@@ -1,45 +1,60 @@
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework.exceptions import NotFound, PermissionDenied
+from rest_framework.permissions import IsAuthenticated
 from .models import Documento
 from .serializers import DocumentoSerializer
-from rest_framework.exceptions import AuthenticationFailed
-from rest_framework.permissions import IsAuthenticated
 
-permission_classes = [IsAuthenticated]
+class DocumentoDetailView(APIView):
+    permission_classes = [IsAuthenticated]
 
-class DocumentosView(APIView):
-    def get(self, request):
-        print(request.user)
-        documentos = Documento.objects.all()
-        serializer = DocumentoSerializer(documentos, many=True)
+    def get_documento(self, documento_id):
+        try:
+            return Documento.objects.get(id=documento_id, user=self.request.user)
+        except Documento.DoesNotExist:
+            raise NotFound("Documento not found")
+
+    def get(self, request, documento_id):
+        documento = self.get_documento(documento_id)
+        serializer = DocumentoSerializer(documento)
         return Response(serializer.data)
-    
 
     def post(self, request):
-        
-        # if not request.user.is_authenticated:
-        #     return Response({"message": "Authentication required"}, status=status.HTTP_401_UNAUTHORIZED)
-
         serializer = DocumentoSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
+            serializer.save(user=request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    # def post(self, request):
-    #     # Verifica se o usuário está autenticado
-    #     if not request.user.is_authenticated:
-    #         raise AuthenticationFailed('Autenticação necessária')
+    def put(self, request, documento_id):
+        documento = self.get_documento(documento_id)
+        serializer = DocumentoSerializer(documento, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    #     # Serializa os dados do documento recebidos na requisição
-    #     serializer = DocumentoSerializer(data=request.data)
+    def delete(self, request, documento_id):
+        documento = self.get_documento(documento_id)
+        documento.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
-    #     # Valida os dados do documento
-    #     if serializer.is_valid():
-    #         # Salva o documento no banco de dados associado ao usuário autenticado
-    #         serializer.save(user=request.user)
-    #         # Retorna os dados do documento criado com o status 201 Created
-    #         return Response(serializer.data, status=201)
-    #     # Retorna erros de validação caso os dados do documento sejam inválidos
-    #     return Response(serializer.errors, status=400)
+
+class DocumentoListView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get_all_documentos(self):
+        return Documento.objects.filter(user=self.request.user)
+
+    def get(self, request):
+        documentos = self.get_all_documentos()
+        serializer = DocumentoSerializer(documentos, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = DocumentoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
