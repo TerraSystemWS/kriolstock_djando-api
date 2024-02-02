4137fadd7b890667cb8ceaaaaa657a7568991be1
Ailton Duarte <adidas.coyas@gmail.com>
2024-02-02 03:25:22 -0100

    feat: add more data to users

--- kriolstock_dapi/users/models.py
+++ kriolstock_dapi/users/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+# Create your models here.
 class User(AbstractUser):
-    name = models.CharField(max_length=255)
-    email = models.CharField(max_length=255, unique=True)
-    password = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=255)  # hashed password
+    full_name = models.CharField(max_length=255)
+    bio = models.TextField(blank=True, null=True)
+    profile_picture = models.URLField(blank=True, null=True)
     username = None
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
-
-# Create your models here.
